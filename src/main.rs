f30da93c4bfa94780c66c3f89f3543bb37361092
shikai liu <lsk569937453@gmail.com>
2024-12-24 19:13:30 +0800

    Add the s3

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,8 @@ use hyper::HeaderMap;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 use std::collections::HashMap;
-use std::time::Duration;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
-use tokio::time;
 use tracing_appender::rolling;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::Layer;
@@ -45,28 +43,42 @@ fn convert(headers: &HeaderMap<HeaderValue>) -> HashMap<String, String> {
     }
     header_hashmap
 }
-#[instrument]
-async fn echo(
-    req: &mut Request<hyper::body::Incoming>,
+async fn echo_with_error(
+    req: Request<hyper::body::Incoming>,
     remote_ip: String,
     target_url: String,
 ) -> Result<Response<Incoming>, anyhow::Error> {
+    let res = echo(req, remote_ip.clone(), target_url).await;
+    match res {
+        Ok(res) => Ok(res),
+        Err(err) => {
+            info!("Error serving connection: {:?},addr is:{:}", err, remote_ip);
+            Err(err)
+        }
+    }
+}
+#[instrument]
+async fn echo(
+    req: Request<hyper::body::Incoming>,
+    remote_ip: String,
+    target_url: String,
+) -> Result<Response<Incoming>, anyhow::Error> {
+    info!("Received a request from {}", remote_ip);
     let uri = req.uri().clone();
     let path = uri.path().to_string();
     let hash_map = convert(req.headers());
     let mut result_map = HashMap::new();
     result_map.insert("headers", format!("{:?}", hash_map));
     result_map.insert("path", format!("{:?}", path));
-
-    *req.uri_mut() = uri;
-
-    let host = req.uri().host().expect("uri has no host");
-    let port = req.uri().port_u16().unwrap_or(80);
+    info!("{:?}", result_map);
+    let (mut req, b) = req.into_parts();
+    req.uri = target_url.parse().unwrap();
+    let host = req.uri.host().expect("uri has no host");
+    let port = req.uri.port_u16().unwrap_or(80);
     let addr = format!("{}:{}", host, port);
-
     let client_stream = TcpStream::connect(addr).await.unwrap();
     let io = TokioIo::new(client_stream);
-
+    let req = Request::from_parts(req, b);
     let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
     tokio::task::spawn(async move {
         if let Err(err) = conn.await {
@@ -75,6 +87,7 @@ async fn echo(
     });
 
     let res = sender.send_request(req).await?;
+    info!("res code:{},header:{:?}", res.status(), res.headers());
     Ok(res)
 
     // let body = full(format!("{:?}", result_map));
@@ -96,9 +109,14 @@ fn setup_logger() -> Result<(), anyhow::Error> {
         .with_ansi(false)
         .with_writer(app_file)
         .with_filter(tracing_subscriber::filter::LevelFilter::INFO);
-
+    let console_layer = tracing_subscriber::fmt::Layer::new()
+        .with_target(true)
+        .with_ansi(true) // Enable ANSI color codes for console output
+        .with_writer(std::io::stdout) // Log to stdout (console)
+        .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG); //
     tracing_subscriber::registry()
         .with(file_layer)
+        .with(console_layer)
         .with(tracing_subscriber::filter::LevelFilter::TRACE)
         .init();
     Ok(())
@@ -117,16 +135,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     loop {
         let (stream, addr) = listener.accept().await?;
         let addr_str = addr.to_string();
+        let addr_str_clone = addr_str.clone();
+        let target_url_cloned = target_url.clone();
         let io = TokioIo::new(stream);
-        let target_url_clone = target_url.clone();
         tokio::spawn(async move {
-            let addr_str_cloned = addr_str.clone();
             if let Err(err) = http1::Builder::new()
                 .keep_alive(true)
                 .serve_connection(
                     io,
-                    service_fn(move |req: &mut Request<Incoming>| {
-                        echo(req, addr_str_cloned.clone(), target_url_clone.clone())
+                    service_fn(move |req: Request<Incoming>| {
+                        echo_with_error(req, addr_str_clone.clone(), target_url_cloned.clone())
                     }),
                 )
                 .await
